77779435d7d5d62972b86cb3906363bef347e041
Adnan Rao <adnanejaz2874@gmail.com>
2025-06-16 21:55:26 +0500

    today work

---
+++ script2.js
@@ -0,0 +1,14 @@
+
+ // Variable initialization
+    var a = 2, b = 1;
+
+    // Expression evaluation
+    var result = --a - --b + ++b + b--;
+
+    // Display the output
+    document.getElementById("output").innerHTML =
+        "<strong>a:</strong> " + a + "<br>" +
+        "<strong>b:</strong> " + b + "<br>" +
+        "<strong>result:</strong> " + result;
+    
+        
